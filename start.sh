ea493821d8161860765c98df7f07b19ade8d5304
ruaanls <ruanlima257@hotmail.com>
2025-11-06 01:07:53 -0300

    ALterações kafka broker

--- start.sh
+++ start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-set -e  # Encerra se qualquer comando falhar
+set -e
 
 echo "🚀 Iniciando Zookeeper..."
-/opt/kafka/bin/zookeeper-server-start.sh /opt/kafka/config/zookeeper.properties &
+/opt/kafka/bin/zookeeper-server-start.sh -daemon /opt/kafka/config/zookeeper.properties
 sleep 5
 
-echo "🚀 Iniciando Kafka..."
-/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties &
-sleep 10
+echo "🚀 Configurando e iniciando Kafka..."
+HOST_IP=$(hostname -i)
+echo "advertised.listeners=PLAINTEXT://${HOST_IP}:9092" >> /opt/kafka/config/server.properties
+echo "listeners=PLAINTEXT://0.0.0.0:9092" >> /opt/kafka/config/server.properties
 
-echo "✅ Kafka iniciado. Iniciando aplicação Java..."
+# Inicia o Kafka em background
+/opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/server.properties &
+sleep 7
+
+echo "✅ Kafka iniciado com sucesso na porta 9092."
+echo "🚀 Iniciando aplicação Java..."
 exec java -jar app.jar
